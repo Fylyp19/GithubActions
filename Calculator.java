0d7ce6cacbb0132e618d0ae09b54a7acc2aa9a68
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-05-04 22:46:58 +0200

    test kalkulatora

--- Calculator.java
+++ Calculator.java
@@ -1,28 +1,7 @@
 public class Calculator {
+
     public int add(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-}
-
-import org.junit.Test;
-        import static org.junit.Assert.*;
-
-public class CalculatorTest {
-    @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(3, 4);
-        assertEquals(7, result);
-    }
-
-    @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator();
-        int result = calculator.subtract(5, 2);
-        assertEquals(3, result);
-    }
 }
