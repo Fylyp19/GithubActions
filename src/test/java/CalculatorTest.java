9f8727fc261d3371aa313bbea333c19db85ab272
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-05-04 23:15:45 +0200

    124512421

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,7 +5,7 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(-1, 2);
+        int result = calculator.add(-1, -2);
         Assertions.assertTrue(result >= 0, "Wynik dodawania nie może być mniejszy od zera");
     }
 }
